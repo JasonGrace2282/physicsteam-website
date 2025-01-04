f923cfa8c78767a3a9179a880ffb8ba323614e15
JasonGrace2282 <aarush.deshpande@gmail.com>
2025-01-03 21:06:52 -0500

    Add basic scrolling features

--- static/js/nav.js
+++ static/js/nav.js
@@ -13,5 +13,23 @@ $(function() {
   // add active class to current page
   const a = $(`a[goto="${url}"]`);
   a.addClass("text-teal-400").removeClass("text-white");
-  console.log(url);
+
+  // change background of nav on scroll
+  function scrolled() {
+    return window.scrollY > 100;
+  }
+
+  $(window).on("scroll", () => {
+    if (!isMobile()) {
+      if (scrolled()) {
+        $("nav").removeClass("bg-transparent").addClass("bg-white");
+        $("nav .text-white").removeClass("text-white").addClass("text-teal-400");
+        $("nav img").attr("src", "img/logo/bluegreen.png");
+      } else {
+        $("nav").removeClass("bg-white").addClass("bg-transparent");
+        $("nav .text-teal-400").removeClass("text-teal-400").addClass("text-white");
+        $("nav img").attr("src", "img/logo/whitegreen.png");
+      }
+    }
+  });
 })
